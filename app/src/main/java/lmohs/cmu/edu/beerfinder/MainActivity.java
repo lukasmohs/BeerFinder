5a751f9c29c9bcd15cfd93c491c37c541313ec6b
Lukas Mohs <ga47boq@mytum.de>
2017-03-31 17:13:08 -0400

    added location manager

--- app/src/main/java/lmohs/cmu/edu/beerfinder/MainActivity.java
+++ app/src/main/java/lmohs/cmu/edu/beerfinder/MainActivity.java
@@ -1,7 +1,13 @@
 package lmohs.cmu.edu.beerfinder;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,13 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final MainActivity callback = this;
-        new Connection().getBeer(this);
+
+        final LocationListener mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+
+        final Button button = (Button) findViewById(R.id.searchButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                new Connection().getBeer(callback);
+            }
+        });
     }
 
     public void onDownloadReady(String res){
         System.out.println(res);
         ((TextView)findViewById(R.id.textView)).setText(res);
     }
+
+    private
 }
