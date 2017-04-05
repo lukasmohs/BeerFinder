4547160513d3896f7390e412b015d3ed00c27daf
Lukas Mohs <ga47boq@mytum.de>
2017-04-05 17:10:26 -0400

    added device name logging

--- app/src/main/java/lmohs/cmu/edu/beerfinder/Connection.java
+++ app/src/main/java/lmohs/cmu/edu/beerfinder/Connection.java
@@ -1,6 +1,8 @@
 package lmohs.cmu.edu.beerfinder;
 
 import android.os.AsyncTask;
+import android.os.Build;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -21,7 +23,7 @@ public class Connection {
     public void getBeer(MainActivity callback, double lat, double lon, int radius) {
         instance = callback;
         String os = "Android " + System.getProperty("os.version");
-        new AsyncBackendCall().execute(lat+"", lon+"", radius + "", os);
+        new AsyncBackendCall().execute(lat+"", lon+"", radius + "", os,  getDeviceName());
 
     }
 
@@ -39,10 +41,11 @@ public class Connection {
             String res = "";
             try {
                 url = new URL(SERVICEURL
-                        + "?lat=" + params[0]
-                        + "&lon=" + params[1]
-                        + "&radius="+ params[2]
-                        +  "&os=" + params[3]);
+                        + "?lat="       + params[0]
+                        + "&lon="       + params[1]
+                        + "&radius="    + params[2]
+                        + "&os="        + params[3]
+                        + "&device="    + params[4]);
                 urlConnection = (HttpURLConnection) url
                         .openConnection();
 
@@ -64,6 +67,37 @@ public class Connection {
                 }
             }
             return res;
+        }
+    }
+
+    /**
+     * FROM GITHUB:
+     * @return
+     */
+    private String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+    /**
+     * FROM: GITHUB
+     * @param s
+     * @return
+     */
+    private String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
         }
     }
 
